c690469a2cdc132d083486727b37cc07d854795d
Langenhagen <andreas.langenhagen@here.com>
2017-06-12 14:53:18 +0100

    Updated CVs

--- js/Scenes.js
+++ js/Scenes.js
@@ -141,7 +141,7 @@ and the source code on <a target=\"_blank\" href=\"https://github.com/langenhage
 </p>";
 
 var VitaHTML =
-" <a id=\"vita_link_left\" target=\"_blank\" href=\"./res/Vita_DE.pdf\">[DE]</a> <a id=\"vita_link_right\" target=\"_blank\" href=\"./res/Vita_EN.pdf\">[EN]</a>";
+" <a id=\"vita_link_left\" target=\"_blank\" href=\"./res/CV-de.pdf\">[DE]</a> <a id=\"vita_link_right\" target=\"_blank\" href=\"./res/CV-en.pdf\">[EN]</a>";
 
 var AboutHTML =
 " <br> \ <p style=\"padding: 34px;\"> \
